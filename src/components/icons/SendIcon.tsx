7c6eb6eb82f1c7eb786753653a9c0b8f457c3ee6
vievvgit <hello@vievv.net>
2024-04-14 19:11:54 +0200

    css.1.2

--- src/components/icons/SendIcon.tsx
+++ src/components/icons/SendIcon.tsx
@@ -2,6 +2,6 @@ import { JSX } from 'solid-js/jsx-runtime';
 const defaultButtonColor = '#3B81F6';
 export const SendIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="19px" style={{ fill: props.color ?? defaultButtonColor }} {...props}>
-    <circle class="st0" cx="4.8" cy="256.8" r="4.8"/>
+    <circle class="st0" cx="4.8" cy="256.8" r="4.8" />
   </svg>
 );

900dfd61c95757170c54ac376842415ee288e013
vievvgit <hello@vievv.net>
2024-04-16 16:21:33 +0200

    css.5.04

--- src/components/icons/SendIcon.tsx
+++ src/components/icons/SendIcon.tsx
@@ -1,17 +1,12 @@
 import { JSX } from 'solid-js/jsx-runtime';
 const defaultButtonColor = '#000000';
 export const SendIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="19px" style={{ fill: props.color ?? defaultButtonColor }} {...props}>
-    <g>
-      <g>
-        <path
-          class="st0"
-          d="M6.8,29.1l2.8-3l2.8,3H6.8z M6.4,32.8v-1.3c0-0.1,0-0.1,0.1-0.1h6.2c0.1,0,0.1,0,0.1,0.1v1.3
-			c0,0.1,0,0.1-0.1,0.1H6.5C6.4,32.9,6.4,32.9,6.4,32.8z M5.7,32.8c0,0.5,0.3,0.8,0.8,0.8h6.2c0.5,0,0.8-0.3,0.8-0.8v-1.3
-			c0-0.5-0.3-0.8-0.8-0.8H6.5c-0.5,0-0.8,0.3-0.8,0.8V32.8z M6.5,29.8h6.3c0.6,0,0.8-0.5,0.4-0.9l-3.1-3.3c-0.3-0.3-0.7-0.3-1,0
-			L6,28.9C5.7,29.3,5.9,29.8,6.5,29.8z"
-        />
-      </g>
-    </g>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="19px" style={{ fill: props.color ?? defaultButtonColor }} {...props}>
+<path d="M0.9,242.7h0.3v7.7H0.9V242.7z"/>
+	<path d="M2.1,242.7h0.3v7.7H2.1V242.7z"/>
+	<path d="M3.4,242.7h0.3v7.7H3.4V242.7z"/>
+	<path d="M4.7,242.7H5v7.7H4.7V242.7z"/>
+	<path d="M5.9,242.7h0.3v7.7H5.9V242.7z"/>
+	<path d="M7.2,242.7h0.3v7.7H7.2V242.7z"/>
   </svg>
 );

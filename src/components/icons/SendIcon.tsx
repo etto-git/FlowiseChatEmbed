8942daef41be63f23923e51101571e3cf95f2caa
vievvgit <hello@vievv.net>
2024-04-16 17:05:02 +0200

    css.5.07

--- src/components/icons/SendIcon.tsx
+++ src/components/icons/SendIcon.tsx
@@ -1,15 +1,15 @@
 import { JSX } from 'solid-js/jsx-runtime';
 const defaultButtonColor = '#000000';
 export const SendIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
-<span style="
-    transform: scale(1, 2);
-  /* Safari and Chrome */
-    -webkit-transform: scale(1, 2);
-  /* Firefox */
-    -moz-transform: scale(1, 2);
-  /* IE 9+ */
-    -ms-transform: scale(1, 2);
-  /* Opera */
-    -o-transform: scale(1, 2);
-">||||||</span>
+  <span
+    style={{
+      transform: 'scale(1, 2)',
+      '-webkit-transform': 'scale(1, 2)',
+      '-moz-transform': 'scale(1, 2)',
+      '-ms-transform': 'scale(1, 2)',
+      '-o-transform': 'scale(1, 2)',
+    }}
+  >
+    ||||||
+  </span>
 );

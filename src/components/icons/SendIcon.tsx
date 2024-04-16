022cdad88c6323307a7a973529088e9b50de9655
vievvgit <hello@vievv.net>
2024-04-16 19:06:36 +0200

    css.5.08

--- src/components/icons/SendIcon.tsx
+++ src/components/icons/SendIcon.tsx
@@ -3,11 +3,14 @@ const defaultButtonColor = '#000000';
 export const SendIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   <span
     style={{
-      transform: 'scale(1, 2)',
-      '-webkit-transform': 'scale(1, 2)',
-      '-moz-transform': 'scale(1, 2)',
-      '-ms-transform': 'scale(1, 2)',
-      '-o-transform': 'scale(1, 2)',
+      transform: 'scale(1, 3.2)',
+      '-webkit-transform': 'scale(1,3.2)',
+      '-moz-transform': 'scale(1, 3.2)',
+      '-ms-transform': 'scale(1, 3.2)',
+      '-o-transform': 'scale(1, 3.2)',
+      'letter-spacing':'1pt',
+      'margin-right':'2px',
+
     }}
   >
     ||||||
